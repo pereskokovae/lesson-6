650e5e5dcba858b4e055f004e947ce8190b81289
pereskokovae <pereskokovae48@gmail.com>
2024-11-26 23:26:11 +0700

    Добавила библиотеку и структурировала код

--- main.py
+++ main.py
@@ -1,48 +1,42 @@
-PASSWORD = input("Введите пароль: ")
+import urwid
 
 
-def main():
+def is_very_long(password):
+	symbol = len(str(password))
+	is_very_long = symbol > 8
+	return is_very_long
+def has_digit(password):
+	return any(letter.isdigit() for letter in password)
+def has_letters(password):
+	return any(letter.isalpha() for letter in password)
+def has_upper_letters(password):
+	return any(letter.isupper() for letter in password)
+def has_lower_letters(password):
+	return any(letter.islower() for letter in password)
+def has_symbols(password):
+	return any(not letter.isdigit() and not letter.isalpha() for letter in password)
+
+
+def on_ask_change(edit, password):
 	score = 0
-
-
-	def is_very_long(PASSWORD):
-		symbol = len(PASSWORD)
-		is_very_long = symbol > 8
-		return is_very_long
-	def has_digit(PASSWORD):
-		return any(letter.isdigit() for letter in PASSWORD)
-		return has_digit
-	def has_letters(PASSWORD):
-		return any(letter.isalpha() for letter in PASSWORD)
-		return has_letters
-	def has_upper_letters(PASSWORD):
-		return any(letter.isupper() for letter in PASSWORD)
-		return has_upper_letters
-	def has_lower_letters(PASSWORD):
-		return any(letter.islower() for letter in PASSWORD)
-		return has_lower_letters
-	def has_symbols(PASSWORD):
-		return any(not letter.isdigit() and not letter.isalpha() for letter in PASSWORD)
-		return has_symbols
-
-
 	rating_functions = [
-		is_very_long(PASSWORD),
-		has_digit(PASSWORD),
-		has_letters(PASSWORD),
-		has_upper_letters(PASSWORD),
-		has_lower_letters(PASSWORD),
-		has_symbols(PASSWORD)
-]
-
-
+		is_very_long(password),
+		has_digit(password),
+		has_letters(password),
+		has_upper_letters(password),
+		has_lower_letters(password),
+		has_symbols(password)
+	]
 	for code_counting in rating_functions:
-		if code_counting:
+		if code_counting == True:
 			score = score + 2
+	reply.set_text("Рейтинг пароля: " + str(score))
 
-
-	print(f"Рейтинг пароля: {score}")
-		
 
 if __name__ == "__main__":
-	main()
+	ask = urwid.Edit("Введите пароль: ", mask='*')
+	reply = urwid.Text("")
+	menu = urwid.Pile([ask, reply])
+	menu = urwid.Filler(menu, valign='top')
+	urwid.connect_signal(ask, 'change', on_ask_change)
+	urwid.MainLoop(menu).run()
